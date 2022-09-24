56d05da5b48992c7fdcc526678450469cbb5a014
maru9 <111681519+klfn9@users.noreply.github.com>
2022-09-24 10:36:26 +0000

    bookers2finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,71 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
+
+  before_action :authenticate_user!
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book), notice: 'You have created book successfully.'
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
-  
+
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
+  end
+
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == correct_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+ get 'home/about' => 'homes#about'
+ get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-   resources :books, only: [:new, :create, :index, :show]
+   resources :books, only: [:create, :index, :show, :update, :destroy, :edit]
+   resources :users, only: [:show, :edit, :index, :update]
 end
